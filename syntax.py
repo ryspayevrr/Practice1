faba6f89b2c0acbeff9a4adfd41b0acc771c4415
ryspayevrr <ratmir.ryspaew164@gmail.com>
2026-01-20 12:39:05 +0500

    Checked for another error

--- syntax.py
+++ syntax.py
@@ -1,4 +1,3 @@
 if 5 > 2:
- print("Five is greater than two!") 
-if 5 > 2:
-        print("Five is greater than two!") 
+ print("Five is greater than two!")
+        print("Five is greater than two!")
